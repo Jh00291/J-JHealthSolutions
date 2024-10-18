0d7df7d5414d18c49e984be5f1905bb92f01adbc
Jason Nunez <55280347+JasonNunez@users.noreply.github.com>
2024-10-17 22:04:06 -0700

    Create the Initial Project

---
+++ code/J-JHealthSolutions/DAL/Connection.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace J_JHealthSolutions.DAL
+{
+    public static class Connection
+    {
+        public static string ConnectionString()
+        {
+            var builder = new MySqlConnectionStringBuilder
+            {
+                Server = "cs-dblab01.uwg.westga.edu",
+                Database = "cs3230f24a",
+                UserID = "cs3230f24a",
+                Password = "tex.U47VGb>*eq.VQ)K{",
+                Port = 3306
+            };
+
+            return builder.ToString();
+
+        }
+    }
+}

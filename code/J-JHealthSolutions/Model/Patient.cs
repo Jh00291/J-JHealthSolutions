cf6d83b7a3296aadc4d883a68dd40ee5045fca5f
Jason Nunez <55280347+JasonNunez@users.noreply.github.com>
2024-10-19 11:26:13 -0700

    Create the Patient class

--- code/J-JHealthSolutions/Model/Patient.cs
+++ code/J-JHealthSolutions/Model/Patient.cs
@@ -8,5 +8,36 @@ namespace J_JHealthSolutions.Model
 {
     public class Patient
     {
+        public int PatientId { get; set; }     
+        public string FName { get; set; }        
+        public string LName { get; set; }      
+        public DateTime Dob { get; set; }      
+        public string Address1 { get; set; }   
+        public string Address2 { get; set; }   
+        public string City { get; set; }       
+        public string State { get; set; }         
+        public string Zipcode { get; set; }
+        public string Phone { get; set; }           
+        public bool Active { get; set; }        
+
+        public Patient()
+        {
+        }
+
+        public Patient(int patientId, string fName, string lName, DateTime dob, string address1,
+            string address2, string city, string state, string zipcode, string phone, bool active)
+        {
+            PatientId = patientId;
+            FName = fName;
+            LName = lName;
+            Dob = dob;
+            Address1 = address1;
+            Address2 = address2;
+            City = city;
+            State = state;
+            Zipcode = zipcode;
+            Phone = phone;
+            Active = active;
+        }
     }
 }

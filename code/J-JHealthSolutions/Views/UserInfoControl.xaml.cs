1a3dcf17520fa6d2c921e55579b380f3ad12f892
Jason Nunez <55280347+JasonNunez@users.noreply.github.com>
2024-10-20 04:04:40 -0700

    Create a UserInfoControl that will be responsible for showcasing the current user's information

---
+++ code/J-JHealthSolutions/Views/UserInfoControl.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace J_JHealthSolutions.Views
+{
+    /// <summary>
+    /// Interaction logic for UserInfoControl.xaml
+    /// </summary>
+    public partial class UserInfoControl : UserControl
+    {
+        public UserInfoControl()
+        {
+            InitializeComponent();
+            this.DataContext = this;
+        }
+
+        public static readonly DependencyProperty UserIdProperty =
+            DependencyProperty.Register("UserId", typeof(string), typeof(UserInfoControl));
+
+        public string UserId
+        {
+            get { return (string)GetValue(UserIdProperty); }
+            set { SetValue(UserIdProperty, value); }
+        }
+
+        public static readonly DependencyProperty FullNameProperty =
+            DependencyProperty.Register("FullName", typeof(string), typeof(UserInfoControl));
+
+        public string FullName
+        {
+            get { return (string)GetValue(FullNameProperty); }
+            set { SetValue(FullNameProperty, value); }
+        }
+    }
+}

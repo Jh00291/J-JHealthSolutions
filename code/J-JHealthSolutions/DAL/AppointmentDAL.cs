04ec9aff8b2e772b16bfdc134bd231c5f712eba5
Jason Nunez <55280347+JasonNunez@users.noreply.github.com>
2024-10-29 12:40:37 -0400

    Refactor AppointmentDal and add validation checks so that it checks if the selected Patient or Doctor is in the database

--- code/J-JHealthSolutions/DAL/AppointmentDAL.cs
+++ code/J-JHealthSolutions/DAL/AppointmentDAL.cs
@@ -1,40 +1,64 @@
 ﻿using J_JHealthSolutions.Model;
 using MySql.Data.MySqlClient;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace J_JHealthSolutions.DAL
 {
-    public class AppointmentDAL
+    public class AppointmentDal
     {
         /// <summary>
-        /// Adds a new appointment to the database.
+        /// Adds a new appointment to the database after validating PatientId and DoctorId.
         /// </summary>
         /// <param name="appointment">The Appointment object to add.</param>
         /// <returns>The generated appointment_id.</returns>
         public int AddAppointment(Appointment appointment)
         {
             using var connection = new MySqlConnection(Connection.ConnectionString());
-
             connection.Open();
-            var query = @"INSERT INTO Appointment (patient_id, doctor_id, `datetime`, reason, `status`)
-                          VALUES (@patientId, @doctorId, @datetime, @reason, @status);
-                          SELECT LAST_INSERT_ID();";
 
-            using var command = new MySqlCommand(query, connection);
-            command.Parameters.Add("@patientId", MySqlDbType.Int32).Value = appointment.PatientId;
-            command.Parameters.Add("@doctorId", MySqlDbType.Int32).Value = appointment.DoctorId;
-            command.Parameters.Add("@datetime", MySqlDbType.DateTime).Value = appointment.DateTime;
-            command.Parameters.Add("@reason", MySqlDbType.VarChar).Value = appointment.Reason;
-            command.Parameters.Add("@status", MySqlDbType.VarChar).Value = appointment.Status;
+            using var transaction = connection.BeginTransaction();
 
-            var generatedId = Convert.ToInt32(command.ExecuteScalar());
-            appointment.AppointmentId = generatedId;
+            try
+            {
+                var patientExistsQuery = "SELECT COUNT(1) FROM Patient WHERE patient_id = @patientId;";
+                using var patientCommand = new MySqlCommand(patientExistsQuery, connection, transaction);
+                patientCommand.Parameters.Add("@patientId", MySqlDbType.Int32).Value = appointment.PatientId;
 
-            return generatedId;
+                var patientExists = Convert.ToInt32(patientCommand.ExecuteScalar()) > 0;
+                if (!patientExists)
+                    throw new Exception($"PatientId {appointment.PatientId} does not exist.");
+
+                var doctorExistsQuery = "SELECT COUNT(1) FROM Doctor WHERE doctor_id = @doctorId;";
+                using var doctorCommand = new MySqlCommand(doctorExistsQuery, connection, transaction);
+                doctorCommand.Parameters.Add("@doctorId", MySqlDbType.Int32).Value = appointment.DoctorId;
+
+                var doctorExists = Convert.ToInt32(doctorCommand.ExecuteScalar()) > 0;
+                if (!doctorExists)
+                    throw new Exception($"DoctorId {appointment.DoctorId} does not exist.");
+
+                var insertQuery = @"INSERT INTO Appointment (patient_id, doctor_id, `datetime`, reason, `status`)
+                                    VALUES (@patientId, @doctorId, @datetime, @reason, @status);
+                                    SELECT LAST_INSERT_ID();";
+
+                using var insertCommand = new MySqlCommand(insertQuery, connection, transaction);
+                insertCommand.Parameters.Add("@patientId", MySqlDbType.Int32).Value = appointment.PatientId;
+                insertCommand.Parameters.Add("@doctorId", MySqlDbType.Int32).Value = appointment.DoctorId;
+                insertCommand.Parameters.Add("@datetime", MySqlDbType.DateTime).Value = appointment.DateTime;
+                insertCommand.Parameters.Add("@reason", MySqlDbType.VarChar).Value = appointment.Reason;
+                insertCommand.Parameters.Add("@status", MySqlDbType.VarChar).Value = appointment.Status;
+
+                var generatedId = Convert.ToInt32(insertCommand.ExecuteScalar());
+                appointment.AppointmentId = generatedId;
+
+                transaction.Commit();
+
+                return generatedId;
+            }
+            catch
+            {
+                transaction.Rollback();
+                throw;
+            }
         }
 
         /// <summary>
@@ -48,29 +72,55 @@ namespace J_JHealthSolutions.DAL
                 throw new ArgumentException("AppointmentId cannot be null for update operation.");
 
             using var connection = new MySqlConnection(Connection.ConnectionString());
-
             connection.Open();
-            var query = @"UPDATE Appointment
-                          SET patient_id = @patientId,
-                              doctor_id = @doctorId,
-                              `datetime` = @datetime,
-                              reason = @reason,
-                              `status` = @status
-                          WHERE appointment_id = @appointmentId;";
 
-            using var command = new MySqlCommand(query, connection);
-            command.Parameters.Add("@patientId", MySqlDbType.Int32).Value = appointment.PatientId;
-            command.Parameters.Add("@doctorId", MySqlDbType.Int32).Value = appointment.DoctorId;
-            command.Parameters.Add("@datetime", MySqlDbType.DateTime).Value = appointment.DateTime;
-            command.Parameters.Add("@reason", MySqlDbType.VarChar).Value = appointment.Reason;
-            command.Parameters.Add("@status", MySqlDbType.VarChar).Value = appointment.Status;
-            command.Parameters.Add("@appointmentId", MySqlDbType.Int32).Value = appointment.AppointmentId;
+            using var transaction = connection.BeginTransaction();
 
-            var affectedRows = command.ExecuteNonQuery();
-            return affectedRows > 0;
+            try
+            {
+                var patientExistsQuery = "SELECT COUNT(1) FROM Patient WHERE patient_id = @patientId;";
+                using var patientCommand = new MySqlCommand(patientExistsQuery, connection, transaction);
+                patientCommand.Parameters.Add("@patientId", MySqlDbType.Int32).Value = appointment.PatientId;
+
+                var patientExists = Convert.ToInt32(patientCommand.ExecuteScalar()) > 0;
+                if (!patientExists)
+                    throw new Exception($"PatientId {appointment.PatientId} does not exist.");
+
+                var doctorExistsQuery = "SELECT COUNT(1) FROM Doctor WHERE doctor_id = @doctorId;";
+                using var doctorCommand = new MySqlCommand(doctorExistsQuery, connection, transaction);
+                doctorCommand.Parameters.Add("@doctorId", MySqlDbType.Int32).Value = appointment.DoctorId;
+
+                var doctorExists = Convert.ToInt32(doctorCommand.ExecuteScalar()) > 0;
+                if (!doctorExists)
+                    throw new Exception($"DoctorId {appointment.DoctorId} does not exist.");
+                
+                var updateQuery = @"UPDATE Appointment
+                                    SET patient_id = @patientId,
+                                        doctor_id = @doctorId,
+                                        `datetime` = @datetime,
+                                        reason = @reason,
+                                        `status` = @status
+                                    WHERE appointment_id = @appointmentId;";
+
+                using var updateCommand = new MySqlCommand(updateQuery, connection, transaction);
+                updateCommand.Parameters.Add("@patientId", MySqlDbType.Int32).Value = appointment.PatientId;
+                updateCommand.Parameters.Add("@doctorId", MySqlDbType.Int32).Value = appointment.DoctorId;
+                updateCommand.Parameters.Add("@datetime", MySqlDbType.DateTime).Value = appointment.DateTime;
+                updateCommand.Parameters.Add("@reason", MySqlDbType.VarChar).Value = appointment.Reason;
+                updateCommand.Parameters.Add("@status", MySqlDbType.VarChar).Value = appointment.Status;
+                updateCommand.Parameters.Add("@appointmentId", MySqlDbType.Int32).Value = appointment.AppointmentId;
+
+                var affectedRows = updateCommand.ExecuteNonQuery();
+
+                transaction.Commit();
+
+                return affectedRows > 0;
+            }
+            catch
+            {
+                transaction.Rollback();
+                throw;
+            }
         }
     }
-}
-
-
 }

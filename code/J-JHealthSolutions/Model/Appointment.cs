da3bdcb5c271433cbd3cb651bac262d750e71116
Jason Nunez <55280347+JasonNunez@users.noreply.github.com>
2024-10-29 12:26:16 -0400

    Create the Appointment class to hold information about an appointment

---
+++ code/J-JHealthSolutions/Model/Appointment.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace J_JHealthSolutions.Model
+{
+    public class Appointment
+    {
+        public int? AppointmentId { get; set; }
+        public int PatientId { get; set; }
+        public int DoctorId { get; set; }
+        public DateTime DateTime { get; set; }
+        public string Reason { get; set; }
+        public string Status { get; set; }
+    }
+
+}

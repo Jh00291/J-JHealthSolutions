554ae7b746840eddf83e5a0548db4c5656f4c9c8
Jason Nunez <55280347+JasonNunez@users.noreply.github.com>
2024-11-02 08:11:40 -0400

    Create a Doctor class representing a Doctor

---
+++ code/J-JHealthSolutions/Model/Doctor.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace J_JHealthSolutions.Model
+{
+    class Doctor : Employee
+    {
+        public int DoctorId { get; set; }
+
+        public Doctor(int? userId, string fName, string lName, DateTime dob, char gender,
+            string address1, string address2, string city, string state, string zipcode, string personalPhone, int doctorId)
+            : base(userId, fName, lName, dob, gender, address1, address2, city, state, zipcode, personalPhone)
+        {
+            DoctorId = doctorId;
+        }
+    }
+}

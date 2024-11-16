09a526986b2e44170f07f4104a784125ff10ed35
Jason Nunez <55280347+JasonNunez@users.noreply.github.com>
2024-11-15 19:59:44 -0500

    Create an enumeration that holds the various valid unit of measurements for a test.

---
+++ code/J-JHealthSolutions/Model/UnitOfMeasure.cs
@@ -0,0 +1,55 @@
+﻿using System.ComponentModel;
+
+namespace J_JHealthSolutions.Model;
+
+/// <summary>
+/// Represents the unit of measure for various hospital tests.
+/// </summary>
+public enum UnitOfMeasure
+{
+    [Description("mg/dL")]
+    MilligramsPerDeciliter,
+
+    [Description("mmHg")]
+    MillimetersOfMercury,
+
+    [Description("kg/m²")]
+    KilogramsPerSquareMeter,
+
+    [Description("bpm")]
+    BeatsPerMinute,
+
+    [Description("L")]
+    Liters,
+
+    [Description("mL")]
+    Milliliters,
+
+    [Description("μg")]
+    Micrograms,
+
+    [Description("ng/mL")]
+    NanogramsPerMilliliter,
+
+    [Description("cells/μL")]
+    CellsPerMicroliter,
+
+    [Description("IU")]
+    InternationalUnits,
+
+    [Description("%")]
+    Percentage,
+
+    [Description("g/dL")]
+    GramsPerDeciliter,
+
+    [Description("Units/L")]
+    UnitsPerLiter,
+
+    [Description("mol/L")]
+    MolesPerLiter,
+
+    [Description("mEq/L")]
+    MilliequivalentsPerLiter,
+
+}

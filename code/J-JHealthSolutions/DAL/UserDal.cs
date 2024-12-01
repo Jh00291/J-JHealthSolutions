1209d27760b5042a220176579d620d594bdd5a07
Jacob Scott Haas <148472456+Jh00291@users.noreply.github.com>
2024-12-01 09:36:08 -0500

    So after running the MigratePasswords method one time. The passwords on the DB side are now hashed.

--- code/J-JHealthSolutions/DAL/UserDal.cs
+++ code/J-JHealthSolutions/DAL/UserDal.cs
@@ -1,5 +1,6 @@
 ﻿using System;
-using System.Data;
+using System.Security.Cryptography;
+using System.Text;
 using Dapper;
 using J_JHealthSolutions.Model;
 using MySql.Data.MySqlClient;
@@ -8,16 +9,22 @@ namespace J_JHealthSolutions.DAL
 {
     public class UserDal
     {
-        /// <summary>
-        /// Attempt to log in a user by verifying their username and password.
-        /// </summary>
-        /// <param name="username">The username</param>
-        /// <param name="password">The password (should be hashed in a real scenario)</param>
-        /// <returns>The User object if credentials are correct, otherwise null</returns>
-        public User Login(string username, string password)
+        // Hashes a password using SHA256
+        private string HashPassword(string password)
+        {
+            using var sha256 = SHA256.Create();
+            byte[] hashedBytes = sha256.ComputeHash(Encoding.UTF8.GetBytes(password));
+            return Convert.ToBase64String(hashedBytes);
+        }
+
+        // Login method: Verifies username and hashed password
+        public User Login(string username, string plainTextPassword)
         {
             using var connection = new MySqlConnection(Connection.ConnectionString());
             connection.Open();
+
+            // Hash the input password
+            string hashedPassword = HashPassword(plainTextPassword);
 
             var query = @"
                 SELECT 
@@ -28,14 +35,53 @@ namespace J_JHealthSolutions.DAL
                     e.l_name AS Lname
                 FROM User u
                 INNER JOIN Employee e ON u.user_id = e.user_id
-                WHERE u.username = @username AND u.password = @password;
+                WHERE u.username = @username AND u.password = @hashedPassword;
             ";
 
-            var parameters = new { username, password };
+            var parameters = new { username, hashedPassword };
 
-            var user = connection.QuerySingleOrDefault<User>(query, parameters);
+            return connection.QuerySingleOrDefault<User>(query, parameters);
+        }
 
-            return user;
+        // Adds a new user with hashed password
+        public void AddUser(User user, string plainTextPassword)
+        {
+            using var connection = new MySqlConnection(Connection.ConnectionString());
+            connection.Open();
+
+            string hashedPassword = HashPassword(plainTextPassword);
+
+            var query = @"
+                INSERT INTO User (username, password, role)
+                VALUES (@username, @hashedPassword, @role);
+            ";
+
+            var parameters = new
+            {
+                username = user.Username,
+                hashedPassword,
+                role = user.Role
+            };
+
+            connection.Execute(query, parameters);
+        }
+
+        // Migrates existing plaintext passwords to hashed passwords
+        public void MigratePasswords()
+        {
+            using var connection = new MySqlConnection(Connection.ConnectionString());
+            connection.Open();
+
+            var users = connection.Query<dynamic>("SELECT user_id, password FROM User;");
+
+            foreach (var user in users)
+            {
+                string hashedPassword = HashPassword(user.password);
+                connection.Execute(
+                    "UPDATE User SET password = @hashedPassword WHERE user_id = @userId;",
+                    new { hashedPassword, userId = user.user_id }
+                );
+            }
         }
     }
 }
